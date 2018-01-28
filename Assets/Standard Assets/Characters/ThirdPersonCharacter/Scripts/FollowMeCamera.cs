f2bfa11886bea17502d7a48e1fa46a7797225b9a
Paul Vazquez <pev@live.com.mx>
2018-01-27 18:57:28 -0600

    Test del Network Character

---
+++ Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/FollowMeCamera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class FollowMeCamera : NetworkBehaviour
+{
+    public float damping = 1;
+    Vector3 offset;
+
+    void Start()
+    {
+        offset = this.transform.position - transform.position;
+    }
+
+    void LateUpdate()
+    {
+        float currentAngle = transform.eulerAngles.y;
+        float desiredAngle = this.transform.eulerAngles.y;
+        float angle = Mathf.LerpAngle(currentAngle, desiredAngle, Time.deltaTime * damping);
+
+        Quaternion rotation = Quaternion.Euler(0, angle, 0);
+        transform.position = this.transform.position - (rotation * offset);
+
+        transform.LookAt(this.transform);
+    }
+}
