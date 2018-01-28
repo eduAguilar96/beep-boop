692f28e933ea447313d7b558ced4c34f84e95b60
eduAguilar96 <eduxx77@gmail.com>
2018-01-27 18:51:31 -0600

    Add button/door logic

---
+++ Assets/Scripts/door.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class door : MonoBehaviour {
+
+    public static int buttonCount = 0;
+    static int buttonsRequired = 3;
+
+	// Use this for initialization
+	void Start () {
+        buttonCount = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (buttonCount >= buttonsRequired) {
+            DestroyObject(gameObject);
+        }
+	}
+}
---
+++ Assets/Scripts/tower_button.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tower_button : MonoBehaviour {
+
+    private bool turnedOn = false;
+    Material m_Material;
+
+    // Use this for initialization
+    void Start () {
+        turnedOn = false;
+        m_Material = GetComponent<Renderer>().material;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && !turnedOn) {
+            turnedOn = true;
+            door.buttonCount++;
+            DestroyObject(gameObject);
+        }
+    }
+}
